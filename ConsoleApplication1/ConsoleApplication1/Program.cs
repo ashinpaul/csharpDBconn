9d923c809b7f46500ece47839bc25b81c310d699
ashin paul <ashinpaul46@gmail.com>
2023-07-11 12:21:35 +0530

    ok

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySqlConnector;
+using System.Data;
 
 namespace ConsoleApplication1
 {
@@ -11,17 +12,13 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string connstr = "server=localhost;
-            user = root;
-            database = sys;
-            port = 3306;
-            password=rajagiri";
+            string connstr = "server=localhost;user = root; database = sys; port = 3306; password=root";
             MySqlConnection conn = new MySqlConnection(connstr);
             try
             {
                 Console.WriteLine("Connection to sql");
                 conn.Open();
-            }
+            }catch(Exception) { }
         }
     }
 }
