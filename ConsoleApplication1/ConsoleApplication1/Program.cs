50ef52cce2ef77a000f9f5dfc596cdeeb88357ee
ashin paul <ashinpaul46@gmail.com>
2023-07-11 11:45:09 +0530

    okk

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string connstr = "server=localhost;
+            user = root;
+            database = sys;
+            port = 3306;
+            password=rajagiri";
+            MySqlConnection conn = new MySqlConnection(connstr);
+            try
+            {
+                Console.WriteLine("Connection to sql");
+                conn.Open();
+            }
         }
     }
 }
